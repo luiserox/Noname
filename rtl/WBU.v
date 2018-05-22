3e5f0b493845c87f4263be79652a153d0055ac10
luiserox <39312187+luiserox@users.noreply.github.com>
2018-05-22 19:13:53 -0400

    Add files via upload

--- rtl/WBU.v
+++ rtl/WBU.v
@@ -4,11 +4,22 @@
 * Luis Ruiz
 */
 
-module WBU(clk_i, rst_i, wbm_dat_i, wbm_addr_i, wbm_sel_i, wbm_we_i, wbm_re_i, wbm_ack_o, wbm_dat_o, wbm_err_o,  
+module WBU(clk_i, rst_i, wbm_dat_i, wbm_addr_i, wbm_sel_i, wbm_we_i, wbm_re_i, wbm_kill_i, wbm_ack_o, wbm_dat_o, wbm_err_o,  
 wbs_dat_i, wbs_ack_i, wbs_err_i, wbs_cyc_o, wbs_stb_o, wbs_dat_o, wbs_addr_o, wbs_we_o, wbs_sel_o);
 
 	input wire clk_i;
 	input wire rst_i;
+
+	//Maquina de estados
+	localparam wbu_state_idle = 9'b000000001;
+	localparam wbu_state_wstart = 9'b000000010;
+	localparam wbu_state_wack = 9'b000000100;
+	localparam wbu_state_wend = 9'b000001000;
+	localparam wbu_state_rstart = 9'b000010000;
+	localparam wbu_state_rack = 9'b000100000;
+	localparam wbu_state_rend = 9'b001000000;
+	localparam wbu_state_err = 9'b010000000;
+	localparam wbu_state_kill = 9'b100000000;
 
 	//Comunicacion con pipeline
 	input wire [31:0] wbm_dat_i;
@@ -16,6 +27,7 @@ wbs_dat_i, wbs_ack_i, wbs_err_i, wbs_cyc_o, wbs_stb_o, wbs_dat_o, wbs_addr_o, wb
 	input wire [3:0] wbm_sel_i;
 	input wire wbm_we_i;
 	input wire wbm_re_i;
+	input wire wbm_kill_i;
 	output reg wbm_ack_o;
 	output reg [31:0] wbm_dat_o;
 	output reg wbm_err_o;
@@ -30,23 +42,25 @@ wbs_dat_i, wbs_ack_i, wbs_err_i, wbs_cyc_o, wbs_stb_o, wbs_dat_o, wbs_addr_o, wb
 	output reg [31:0] wbs_addr_o;
 	output reg wbs_we_o;
 	output reg [3:0] wbs_sel_o;
-/*
-*	assign wbm_ack_o = wbs_ack_i;
-*	assign wbm_err_o = wbs_err_i;
-*	assign wbm_dat_o = wbs_dat_i;
-*	assign wbs_dat_o = wbm_dat_i;
-*	assign wbs_addr_o = wbm_addr_i;
-*	assign wbs_we_o = wbm_we_i;
-*/
+
+	reg [8:0] wbu_state;
 
 	always @(posedge clk_i) begin
 		if (rst_i) begin
-
+			wbu_state <= wbu_state_idle;
 		end else begin
-			if(wbm_we_i) begin
-				if(wbs_err_i) begin
-					wbm_err_o <= wbs_err_i;
-				end else if(!wbs_cyc_o && !wbs_stb_o) begin
+			case(wbu_state)
+				wbu_state_idle: begin
+					if(wbm_we_i) begin
+						wbu_state <= wbu_state_wstart;
+					end else if(wbm_re_i) begin
+						wbu_state <= wbu_state_rstart;
+					end
+				end
+				wbu_state_wstart: begin
+					if(wbm_kill_i) begin
+						wbu_state <= wbu_state_kill;
+					end else
 					wbm_err_o <= wbs_err_i;
 					wbs_addr_o <= wbm_addr_i;
 					wbs_dat_o <= wbm_dat_i;
@@ -54,28 +68,79 @@ wbs_dat_i, wbs_ack_i, wbs_err_i, wbs_cyc_o, wbs_stb_o, wbs_dat_o, wbs_addr_o, wb
 					wbs_sel_o <= wbm_sel_i;
 					wbs_cyc_o <= 1;
 					wbs_stb_o <= 1;
-				end else if(wbs_ack_i) begin
-					wbs_cyc_o <= 0;
-					wbs_stb_o <= 0;
+					wbu_state <= wbu_state_wack;
+					end
 				end
-			end else if(wbm_re_i) begin
-				if(wbs_err_i) begin
-					wbm_err_o <= wbs_err_i;
-				end else if(!wbs_cyc_o && !wbs_stb_o) begin
-					wbm_err_o <= wbs_err_i;
-					wbs_addr_o <= wbm_addr_i;
-					wbs_we_o <= 0;
-					wbs_sel_o <= wbm_sel_i;
-					wbs_cyc_o <= 1;
-					wbs_stb_o <= 1;
-				end else if(wbs_ack_i && wbs_stb_o) begin
-					wbs_stb_o <= 0;
-				end else if(wbs_ack_i && !wbs_stb_o) begin
-					wbs_cyc_o <= 0;
-					wbs_dat_o <= wbm_dat_i;
+				wbu_state_wack: begin
+					if(wbm_kill_i) begin
+						wbu_state <= wbu_state_kill;
+					end else if(wbs_err_i) begin
+						wbu_state <= wbu_state_err;
+					end else if(wbs_ack_i) begin
+						wbm_ack_o <= wbs_ack_i;
+						wbu_state <= wbu_state_wend;
+					end
 				end
-			end
-
+				wbu_state_wend: begin
+					if(wbm_kill_i) begin
+						wbu_state <= wbu_state_kill;
+					end else
+						wbs_cyc_o <= 0;
+						wbs_stb_o <= 0;
+						wbu_state <= wbu_state_idle;
+					end
+				end
+				wbu_state_rstart: begin
+					if(wbm_kill_i) begin
+						wbu_state <= wbu_state_kill;
+					end else
+						wbm_err_o <= wbs_err_i;
+						wbs_addr_o <= wbm_addr_i;
+						wbs_we_o <= 0;
+						wbs_sel_o <= wbm_sel_i;
+						wbs_cyc_o <= 1;
+						wbs_stb_o <= 1;
+						wbu_state <= wbu_state_rack;
+					end
+				end
+				wbu_state_rack: begin
+					if(wbm_kill_i) begin
+						wbu_state <= wbu_state_kill;
+					end else
+						wbs_stb_o <= 0;
+						if(wbs_err_i) begin
+							wbm_err_o.next <= wbs_err_i;
+							wbu_state <= wbu_state_idle;
+						end else if(wbs_ack_i) begin
+							wbm_ack_o <= wbs_ack_i;
+							wbu_state <= wbu_state_rend;
+					end
+				end
+				wbu_state_rend: begin
+					if(wbm_kill_i) begin
+						wbu_state <= wbu_state_kill;
+					end else
+						wbs_cyc_o <= 0;
+						wbs_dat_o <= wbm_dat_i;
+						wbu_state <= wbu_state_idle;
+					end
+				end
+				wbu_state_err: begin
+					if(wbm_kill_i) begin
+						wbu_state <= wbu_state_kill;
+					end else
+						wbm_err_o <= wbs_err_i;
+						wbs_cyc_o <= 0;
+						wbs_stb_o <= 0;	
+						wbu_state <= wbu_state_idle;
+					end				
+				end
+				wbu_state_kill: begin
+					wbs_cyc_o <= 0;
+					wbs_stb_o <= 0;	
+					wbu_state <= wbu_state_idle;
+				end
+			endcase
 		end
 
 	end
